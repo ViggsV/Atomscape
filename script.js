b65d6d107ca3f45dd018344571b78fdde3a1d552
ViggsV <130056822+ViggsV@users.noreply.github.com>
2025-04-23 16:15:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+// if scrolled to the top = win
+// if hit a reactive element = lose
+// if hit a non-reactive element = slow down
+// Energy bar = 100%; dodge left or right = -40%; regeneration = wow druid regen
+// Max game length = 5 minutes
+
+
+class Entity {
+    constructor(name, color, pull, size, jitter, reactivity,) {
+
+    }
+}
+
